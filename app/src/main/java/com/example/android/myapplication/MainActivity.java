f8c7b910b4bd9068f8cb2754973cd96570269efb
robertlagrasse <robert.lagrasse@gmail.com>
2016-08-29 10:18:45 -0700

    Create Layout, Button, EditText in Java. Set Attributes Re- Position.

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.graphics.Color;
 
@@ -16,7 +17,12 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout myLayout = new RelativeLayout(this);
         Button myButton = new Button(this);
 
+        //Build an edittext
+        EditText myEditText = new EditText(this);
 
+        // Do some identifying
+        myButton.setId(1);
+        myEditText.setId(2);
 
         // Modify some button attributes
         myButton.setText("Click!");
@@ -25,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         // Modify some layout attributes
         myLayout.setBackgroundColor(Color.BLUE);
 
-        // Do some positioning
+        // Modify some editText attributes
+        myEditText.setText("Here's some text");
 
         //Build a Layout for the button to live in.
         //This is a layout that will wrap the content of whatever is placed inside it.
@@ -34,9 +41,19 @@ public class MainActivity extends AppCompatActivity {
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
 
+        //Build a Layout for the editText to live in.
+        //This is a layout that will wrap the content of whatever is placed inside it.
+        RelativeLayout.LayoutParams editTextDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
         //Anything that applies the buttonDetails object will follow these rules
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        editTextDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        editTextDetails.addRule(RelativeLayout.ABOVE, myButton.getId());
 
         //Display the layout for the user (empty right now)
         setContentView(myLayout);
@@ -44,5 +61,9 @@ public class MainActivity extends AppCompatActivity {
         //Add the button to the layout
         //Passing two objects to the .addView() method. The button, and the rules via buttonDetails.
         myLayout.addView(myButton, buttonDetails);
+
+        //Add the EditText to the layout
+        //Pass the object itself, and the rules
+        myLayout.addView(myEditText, editTextDetails);
     }
 }
